7f7f5157f54a6e167b8dd2353e3f27adfa535642
Santiago <santiago.folgueras@cern.ch>
2020-11-20 12:44:30 +0100

    first commit

---
+++ OMTFTools/cfgs/runMuonOverlapDataDumpPhase2_SF.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("L1TMuonEmulation")
+import os
+import sys
+import commands
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+verbose = True
+dumpHitsFileName = 'OMTFHits_P2TPs' 
+ 
+if verbose: 
+    process.MessageLogger = cms.Service("MessageLogger",
+       #suppressInfo       = cms.untracked.vstring('AfterSource', 'PostModule'),
+       destinations   = cms.untracked.vstring(
+                                               #'detailedInfo',
+                                               #'critical',
+                                               #'cout',
+                                               #'cerr',
+                                               'omtfEventPrint'
+                    ),
+       categories        = cms.untracked.vstring('l1tOmtfEventPrint', 'OMTFReconstruction'),
+       omtfEventPrint = cms.untracked.PSet(    
+                         filename  = cms.untracked.string('log_' + dumpHitsFileName),
+                         extension = cms.untracked.string('.txt'),                
+                         threshold = cms.untracked.string('DEBUG'),
+                         default = cms.untracked.PSet( limit = cms.untracked.int32(0) ), 
+                         #INFO   =  cms.untracked.int32(0),
+                         #DEBUG   = cms.untracked.int32(0),
+                         l1tOmtfEventPrint = cms.untracked.PSet( limit = cms.untracked.int32(1000000000) ),
+                         OMTFReconstruction = cms.untracked.PSet( limit = cms.untracked.int32(1000000000) )
+                       ),
+       debugModules = cms.untracked.vstring('simOmtfPhase2Digis')
+    )
+
+    #process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+if not verbose:
+    process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
+    process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True), 
+                                         #SkipEvent = cms.untracked.vstring('ProductNotFound') 
+                                     )
+
+process.source = cms.Source('PoolSource',
+                            fileNames = cms.untracked.vstring("file:///eos/cms/store/user/folguera/P2L1TUpgrade/Mu_FlatPt2to100-pythia8-gun_file.root")
+)
+	                    
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000))
+
+# PostLS1 geometry used
+process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2015_cff')
+############################
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+
+####Event Setup Producer
+process.load('L1Trigger.L1TMuonOverlapPhase1.fakeOmtfParams_cff')
+process.esProd = cms.EDAnalyzer("EventSetupRecordDataGetter",
+   toGet = cms.VPSet(
+      cms.PSet(record = cms.string('L1TMuonOverlapParamsRcd'),
+               data = cms.vstring('L1TMuonOverlapParams'))
+                   ),
+   verbose = cms.untracked.bool(False)
+)
+
+#process.TFileService = cms.Service("TFileService", fileName = cms.string('omtfAnalysis1.root'), closeFileFast = cms.untracked.bool(True) )
+
+### DT Phase-2 trigger primitives
+process.load("L1Trigger.DTTriggerPhase2.CalibratedDigis_cfi")		
+process.load("L1Trigger.DTTriggerPhase2.dtTriggerPhase2PrimitiveDigis_cfi")		
+
+process.CalibratedDigis.dtDigiTag = "simMuonDTDigis"
+process.CalibratedDigis.scenario = 0
+process.dtTriggerPhase2PrimitiveDigis.scenario = 0
+process.dtTriggerPhase2PrimitiveDigis.scenario = 0 # 0 for mc, 1 for data, 2 for slice test
+								
+####OMTF Emulator
+process.load('L1Trigger.L1TMuonOverlapPhase2.simOmtfPhase2Digis_cfi')
+
+process.simOmtfPhase2Digis.dumpResultToXML = cms.bool(True)
+process.simOmtfPhase2Digis.rpcMaxClusterSize = cms.int32(3)
+process.simOmtfPhase2Digis.rpcMaxClusterCnt = cms.int32(2)
+process.simOmtfPhase2Digis.rpcDropAllClustersIfMoreThanMax = cms.bool(True)
+#process.simOmtfPhase2Digis.minDtPhiQuality  = cms.int32(5)
+#process.simOmtfPhase2Digis.minDtPhiBQuality = cms.int32(10) ## to avoid LQ 
+process.simOmtfPhase2Digis.dumpHitsToROOT = cms.bool(True)
+process.simOmtfPhase2Digis.dumpHitsFileName = cms.string('OMTFHits_P2TP.root')
+process.simOmtfPhase2Digis.eventCaptureDebug = cms.bool(True)
+
+process.simOmtfPhase2Digis.dropDTPrimitives = cms.bool(True)
+process.simOmtfPhase2Digis.usePhase2DTPrimitives = cms.bool(True)
+
+
+process.simOmtfPhase2Digis.lctCentralBx = cms.int32(8);#<<<<<<<<<<<<<<<<!!!!!!!!!!!!!!!!!!!!TODO this was changed in CMSSW 10(?) to 8. if the data were generated with the previous CMSSW then you have to use 6
+
+process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
+process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
+
+process.L1TMuonSeq = cms.Sequence( process.esProd          
+#                                   + process.CalibratedDigis
+#                                   + process.dtTriggerPhase2PrimitiveDigis
+                                   + process.simOmtfPhase2Digis 
+                                   #+ process.dumpED
+                                   #+ process.dumpES
+)
+
+process.L1TMuonPath = cms.Path(process.CalibratedDigis * 
+                               process.dtTriggerPhase2PrimitiveDigis * 
+                               process.L1TMuonSeq)
+
+process.out = cms.OutputModule("PoolOutputModule", 
+                               fileName = cms.untracked.string("l1tomtf_superprimitives2.root"),
+                               outputCommands=cms.untracked.vstring(
+                                   'drop *',
+                                   "keep *_dtTrigger*_*_*",
+                                   "keep *_simDtTrigger*_*_*",
+                                   "keep *_simOmtfPhase2Digis_OMTF_L1TMuonEmulation",
+                                   "keep *_genParticles_*_*", 
+                                   )
+)
+
+#process.output_step = cms.EndPath(process.out)
+#process.schedule = cms.Schedule(process.L1TMuonPath)
+#process.schedule.extend([process.output_step])
---
+++ OMTFTools/crab/multicrab_MC_phase2_P2TP_allQ.py
@@ -0,0 +1,108 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.workArea = 'crab_projects_P2TP_AllQ_Sep16/'
+
+config.section_('JobType')
+config.JobType.psetName = '../L1Trigger/L1TMuonOverlapPhase2/test/runMuonOverlap_SF_allQ.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ["l1tomtf_superprimitives2.root"]
+config.JobType.allowUndistributedCMSSW = True
+
+config.section_('Data')
+config.Data.splitting = 'FileBased'
+config.JobType.maxMemoryMB = 2500
+config.Data.outLFNDirBase = '/store/user/folguera/OMTF/20Sep16/P2TP_allQ/'
+config.Data.publication = False
+
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    from multiprocessing import Process
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+    #############################################################################    
+    MUONGUN=True
+    NUGUN=False
+    DISPLACED=False
+
+    #### Muon Guns
+    if MUONGUN:
+        config.General.requestName = 'L1MuPhase2Ntuples_Mu_FlatPt2to100_PU200'    
+        config.Data.unitsPerJob = 5 #180 #minutes
+        config.Data.inputDataset = '/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW'
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+
+    #### Nu Guns
+    if NUGUN:
+        config.General.requestName = 'L1MuPhase2Ntuples_Nu_E10_PU200'    
+        config.Data.unitsPerJob = 5 #180 #minutes
+        config.Data.inputDataset = '/Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v3/GEN-SIM-DIGI-RAW'
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+
+##SF        config.General.requestName = 'L1MuPhase2Ntuples_Nu_E10_PU140'    
+##SF        config.Data.unitsPerJob = 2
+##SF        config.Data.inputDataset = '/Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU140_106X_upgrade2023_realistic_v3_ext3-v1/GEN-SIM-DIGI-RAW'
+##SF        p = Process(target=submit, args=(config,))
+##SF        p.start()
+##SF        p.join()
+##SF 
+##SF        config.General.requestName = 'L1MuPhase2Ntuples_Nu_E10_PU250'
+##SF        config.Data.unitsPerJob = 2
+##SF        config.Data.inputDataset = '/Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU250_106X_upgrade2023_realistic_v3_ext2-v1/GEN-SIM-DIGI-RAW'
+##SF        p = Process(target=submit, args=(config,))
+##SF        p.start()
+##SF        p.join()
+##SF    
+
+    #### DISPLACED MUON Guns
+    if DISPLACED:
+        config.General.requestName = 'L1MuPhase2Ntuples_DisplacedMuonGun_Pt30To100_Dxy_0_1000'
+        config.Data.unitsPerJob = 5
+        config.Data.inputDataset = '/DisplacedMuons_Pt30to100_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW'
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+        
+        config.General.requestName = 'L1MuPhase2Ntuples_DisplacedMuonGun_Pt2To10_Dxy_0_1000'
+        config.Data.unitsPerJob = 5
+        config.Data.inputDataset = '/DisplacedMuons_Pt2to10_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW'
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+        
+        config.General.requestName = 'L1MuPhase2Ntuples_DisplacedMuonGun_Pt10To30_Dxy_0_1000'
+        config.Data.unitsPerJob = 5
+        config.Data.inputDataset = '/DisplacedMuons_Pt10to30_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW'
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+
+
+
+
+
+
+
+
